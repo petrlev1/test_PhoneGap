a20c2624832302afed8a6f659c75c251f6d823b6
petrlev1 <levendeevp@gmail.com>
2019-08-01 14:24:36 +0300

    100

---
+++ scripts/general.js
@@ -0,0 +1,17 @@
+$( ".logo1" ).click(function() {
+	
+	$(".block1").html("Это приложение на PhoneGap");
+
+});
+
+$( ".but1" ).click(function() {
+	
+	$(".block1").html("Событие 1");
+
+});
+
+$( ".but2" ).click(function() {
+	
+	$(".block1").html("Событие 2");
+
+});
