984ca664e2101bbd007d99122ed08ec656032223
petrlev1 <levendeevp@gmail.com>
2019-08-04 12:56:53 +0300

    100

---
+++ js/modules/catalog.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Модуль каталога
+var catalog = (function($) {
+
+    // Инициализация модуля
+    function init() {
+        _render();
+    }
+
+    // Рендерим каталог
+    function _render() {
+        var template = _.template($('#catalog-template').html()),
+            $goods = $('#goods');
+
+
+		$.getJSON('http://ufk-techno.ru/yml.php', function(data) {
+
+       // $.getJSON('data/goods.json', function(data) {
+            $goods.html(template({goods: data}));
+        });
+    }
+
+
+    // Экспортируем наружу
+    return {
+        init: init
+    }
+    
+})(jQuery);
