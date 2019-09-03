b90550816d63ff7f820f5db363f95850ec28d19d
petrlev1 <levendeevp@gmail.com>
2019-09-03 13:04:23 +0300

    Доработка верстки - кнопка поиска

--- scripts/general.js
+++ scripts/general.js
@@ -1,9 +1,31 @@
 //$('.icFiltr').on('click touch', function(){
 	window.onload = function() {
+		
 		$('.icFiltr').on('click touch', function(){
   //$('.filtersMenu').css("display", "block");
   $('.filtersMenu').toggleClass("filtersMenuAct");
   })
+  
+  $('#inputSearch').on('click touch', function(){
+  $('.inputSearchDown').addClass("filtersMenuAct");
+  })
+  
+  $('#inputSearchBut1').on('click touch', function(){
+  $('.inputSearchDown').removeClass("filtersMenuAct");
+  })
+  
+  $('.productItem1But').on('click touch', function(){
+  $('.filtersMenu').removeClass("filtersMenuAct");
+  })
+
+$('#inputSearchBut1').on('click touch', function(){
+  alert("Поиск работает");
+  })
+  
+  $('#filtersBot').on('click touch', function(){
+  alert("Фильры работают");
+  })
+  
 	}
 //});
 
@@ -11,4 +33,11 @@ $(document).click(function(event) {
   if (!$(event.target).closest(".filtersMenu, .icFiltr").length) {
     $('.filtersMenu').removeClass("filtersMenuAct");
   }
+});
+
+$(document).click(function(event) {
+  if (!$(event.target).closest(".inputSearch").length) {
+    $('.inputSearchDown').removeClass("filtersMenuAct");
+  }
+  
 });
